cb25f0ff7e571c5d0a6cb1f008c8a285d2f98473
Leosantosnunes <leonardon09@gmail.com>
2023-07-16 07:22:51 -0400

    bug fixed

--- incident-management-site/src/app/app.module.ts
+++ incident-management-site/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MovieStoreModule} from './movie-store/movie-store.module';
+import { MovieStoreModule } from './movie-store/movie-store.module';
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './partials/header/header.component';
@@ -12,7 +12,8 @@ import { BasePageComponent } from './partials/base-page/base-page.component';
 import { LibraryComponent } from './pages/library/library.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
-import { MovieStoreComponent } from './movie-store/movie-store.component';
+
+
 
 
 @NgModule({
@@ -21,19 +22,18 @@ import { MovieStoreComponent } from './movie-store/movie-store.component';
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    BasePageComponent,
-    MovieStoreComponent,    
+    BasePageComponent,        
     LibraryComponent,
     AboutComponent,
     ContactComponent
     
     
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    MovieStoreModule,
-    FormsModule
+    AppRoutingModule,    
+    MovieStoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- incident-management-site/src/app/model/static.datasource.ts
+++ incident-management-site/src/app/model/static.datasource.ts
@@ -7,12 +7,12 @@ export class StaticDataSource{
     private movies: Movie[] = [
         new Movie(1, 'Toy Story','John Lasseter','November 22, 1995', 8.3,'01_ts.jpg' ),
         new Movie(2, 'Toy Stdaory','John Lasseter','Novdaember 22, 1995', 8.3,'01_ts.jpg' ),
-        new Movie(3, 'Toy Sadtory','John Lasseter','November 22, 1995', 8.3,'01_ts.jpg' ),
+        new Movie(3, 'Toy Sadtory','John Lasdasseter','November 22, 1995', 8.3,'01_ts.jpg' ),
         new Movie(4, 'Toy Story','Johasdn Lasseter','November 22, 1995', 8.3,'01_ts.jpg' ),
-        new Movie(5, 'Toy Story','John Lasseter','November 22, 1995', 8.3,'01_ts.jpg' ),
+        new Movie(5, 'Toy Story','John Lasssdaeter','November 22, 1995', 8.3,'01_ts.jpg' ),
         new Movie(6, 'Toy Sadtory','John Lasseter','Novasdember 22, 1995', 8.3,'01_ts.jpg' ),
         new Movie(7, 'Toy Story','John Lasseter','November 22, 1995', 8.3,'01_ts.jpg' ),
-        new Movie(8, 'Toy Story','John Lasseter','November 22, 1995', 8.3,'01_ts.jpg' ),
+        new Movie(8, 'Toy Story','John Lasdscsseter','November 22, 1995', 8.3,'01_ts.jpg' ),
         new Movie(9, 'Toy Story','John Lasseter','Novasdember 22, 1995', 8.3,'01_ts.jpg' ),
     ];
 
